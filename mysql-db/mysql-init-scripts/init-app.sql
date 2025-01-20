401381a8e38d08bdac419226eb72444c31f31db2
Agusioma <tcreatesllc@gmail.com>
2025-01-20 14:36:36 +0300

    checkpoint

--- mysql-db/mysql-init-scripts/init-app.sql
+++ mysql-db/mysql-init-scripts/init-app.sql
@@ -1,25 +1,30 @@
-CREATE DATABASE `springserviceDB`;
+-- Create the user 'taa'
+CREATE USER 'taa'@'mysql-db' IDENTIFIED BY '4141';
 
-CREATE DATABASE `logsDB`
+-- Granting all privileges to user 'taa' on all databases
+GRANT ALL PRIVILEGES ON *.* TO 'taa'@'mysql-db' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
 
-CREATE TABLE `logsDB.Logs` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `message` varchar(255) NOT NULL,
-  `timestamp` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-)
+USE microservicesDB;
 
-CREATE TABLE `springserviceDB.animal_visits` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `animal_name` varchar(100) NOT NULL,
-  `reason` varchar(255) NOT NULL,
-  `visit_date` date NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE Logs (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  message VARCHAR(255) NOT NULL,
+  timestamp DATETIME DEFAULT NULL,
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE `springserviceDB.veterinarians` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `specialization` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE animal_visits (
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  animal_name VARCHAR(100) NOT NULL,
+  reason VARCHAR(255) NOT NULL,
+  visit_date DATE NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE veterinarians (
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  specialization VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
 );
